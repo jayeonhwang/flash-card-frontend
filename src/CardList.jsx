3823fc74875bbce1c2402d5b3243fa896cb31c87
Jayeon Hwang <jayeon512@gmail.com>
2024-02-06 12:56:27 -0600

    add pagenation for card slider

--- src/CardList.jsx
+++ src/CardList.jsx
@@ -18,16 +18,44 @@ export function CardList() {
 
   useEffect(getCardList, [id]);
 
+  // pagenations for card slider
+  const [currentPage, setCurrentPage] = useState(1)
+  const cardPerPage = 1;
+
+  const startIndex = (currentPage - 1) * cardPerPage
+  const endIndex = startIndex + cardPerPage
+  const currentCard = Array.isArray(cardLists.cards) ? cardLists.cards.slice(startIndex, endIndex) : [];
+
+  const handlePreviousPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1)
+    }
+    console.log('currentPage:', currentPage)
+  }
+
+  const handleNextPage = () => {
+    const totalPages = Math.ceil(cardLists.cards.length / cardPerPage);
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+      console.log('currentPage:', currentPage);
+    }
+  };
+
+
   return (
     <div>
       <p>Card list</p>
       <h1>{cardLists.title}</h1>
-      {cardLists.cards && cardLists.cards.map(card => (
-        <div key={card.id}>
+      {cardLists.cards && currentCard.map(card => (
+        <div className="card" key={card.id}>
+          <img src={card.image} width="200" />
           <p>{card.word}</p>
           <p>{card.description}</p>
         </div>
+
       ))}
+      <button onClick={handlePreviousPage}>Previous</button>
+      <button onClick={handleNextPage}>Next</button>
     </div>
   )
 }
--- src/Content.jsx
+++ src/Content.jsx
@@ -2,12 +2,14 @@ import axios from "axios";
 import { BundleIndex } from "./BundleIndex";
 import { Routes, Route } from "react-router-dom";
 import { CardList } from "./CardList";
+import { Flip } from "./Flip"
 
 export function Content() {
   return (
     <Routes>
       <Route exact path="/" element={<BundleIndex />} />
       <Route path="/bundles/:id" element={<CardList />} />
+      <Route path="/flip" element={<Flip />} />
     </Routes>
   )
 }
