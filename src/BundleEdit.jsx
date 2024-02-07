9eec55b2a76cbeb1d2fa815a405f06f4489e8f50
Jayeon Hwang <jayeon512@gmail.com>
2024-02-07 12:10:28 -0600

    add adding card action

--- src/BundleEdit.jsx
+++ src/BundleEdit.jsx
@@ -8,6 +8,7 @@ export function BundleEdit() {
   const { id } = useParams();
   console.log(id)
   const [cardLists, setCardLists] = useState([])
+  const [cards, setCards] = useState([])
 
   const getCardList = () => {
     axios.get(`http://localhost:3000/bundles/${id}.json`).then(response => {
@@ -18,12 +19,31 @@ export function BundleEdit() {
 
   useEffect(getCardList, [id]);
 
+  const createCard = (params, successCallback) => {
+    axios.post("http://localhost:3000/cards.json", params).then(response => {
+      setCards([...cards, response.data]);
+      successCallback();
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const params = new FormData(event.target);
+    createCard(params, () => event.target.reset());
+  };
+
 
   return (
     <div>
       <h2>{cardLists.title}</h2>
+      <form className="Create Card" onSubmit={handleSubmit}>
+        <input type="hidden" name="bundle_id" value={cardLists.id} />
+        <p><b>image:</b><input name="image" type="text" /></p>
+        <p><b>Q:</b><input name="word" type="text" /></p>
+        <p><b>A:</b><input name="description" type="text" /></p>
+        <button type="submit">New</button>
+      </form>
 
-      <button>New</button>
       {cardLists.cards && cardLists.cards.map(card => (
         <div key={card.id}>
           {card.word && <p> <b>Q:</b>{card.word}</p>}
